73f5c626c359bc6ab52db86944176c9bfaae649f
lijm1358 <lijm1358@gmail.com>
2023-04-17 04:42:00 +0000

    add models

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+import timm
+
 
 class BaseModel(nn.Module):
     def __init__(self, num_classes):
@@ -50,3 +52,35 @@ class MyModel(nn.Module):
         2. 결과로 나온 output 을 return 해주세요
         """
         return x
+    
+class ResNet18(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.model = timm.create_model("resnet18", pretrained=True, num_classes=num_classes)
+    
+    def forward(self, x):
+        return self.model(x)
+
+class ResNet34(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.model = timm.create_model("resnet34", pretrained=True, num_classes=num_classes)
+    
+    def forward(self, x):
+        return self.model(x)
+    
+class ResNet50(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.model = timm.create_model("resnet50", pretrained=True, num_classes=num_classes)
+    
+    def forward(self, x):
+        return self.model(x)
+    
+class ResNet101(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.model = timm.create_model("resnet101", pretrained=True, num_classes=num_classes)
+    
+    def forward(self, x):
+        return self.model(x)
